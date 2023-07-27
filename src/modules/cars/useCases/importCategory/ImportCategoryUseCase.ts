464c7e7baa0408d530af7f447b20e2efe1bb1694
Pedro Lucas Santos <85002280+PedroLuc-cpu@users.noreply.github.com>
2023-07-26 21:58:39 -0300

    ** Configurando o swagger

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -22,23 +22,21 @@ class ImportCategoryUseCase {
     const [name, description] = line;
     categories.push({ name, description })
    })
-   .on('end', () =>{
-    resolve(categories);
-   }).on('error', (err) => {
-    reject(err);
-   })
-   
- 
-  })
+    .on('end', () => {
+     resolve(categories);
+    }).on('error', (err) => {
+     reject(err);
+    });
+  });
  }
 
-async execute(file: Express.Multer.File): Promise<void> {
+ async execute(file: Express.Multer.File): Promise<void> {
   const categories = await this.loadCategories(file)
-  categories.map( async (category) => {
-   const {name, description} = category;
+  categories.map(async (category) => {
+   const { name, description } = category;
    const existsCategory = this.categoriesRepository.findByName(name);
-   if(!existsCategory){
-    this.categoriesRepository.create({name, description})
+   if (!existsCategory) {
+    this.categoriesRepository.create({ name, description })
    }
   })
  }
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
 import express from "express"
+import swaggerUi from "swagger-ui-express";
+import swaggerFile from './swagger.json';
+
+
 import { categoriesRoutes } from "./Routes/categories.routes";
 import { specificationRoutes } from "./Routes/specifications.routes";
 
 const app = express();
 app.use(express.json());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 app.use("/categories", categoriesRoutes);
 //app.use("/categoriesAll", categoriesRoutes);
