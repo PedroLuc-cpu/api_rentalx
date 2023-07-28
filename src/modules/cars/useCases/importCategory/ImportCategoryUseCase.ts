423d4e236cd3a2412979177dca75cd62ae805433
Pedro Lucas Santos <85002280+PedroLuc-cpu@users.noreply.github.com>
2023-07-27 21:29:54 -0300

    ** finalizado a documentação da API. ** agora quando é feito a importação o arquivo é removido da pasta TMP.

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -3,43 +3,44 @@ import { parse as csvParse } from "csv-parse";
 import { CategoriesRepository } from '../../repositories/CategoriesRepository';
 
 interface IImportCategory {
- name: string;
- description: string;
+  name: string;
+  description: string;
 }
 
 class ImportCategoryUseCase {
- constructor(private categoriesRepository: CategoriesRepository) { }
+  constructor(private categoriesRepository: CategoriesRepository) { }
 
- loadCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
-  return new Promise((resolve, reject) => {
-   const stream = fs.createReadStream(file.path);
-   const categories: IImportCategory[] = [];
-   const parseFile = csvParse();
+  loadCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
+    return new Promise((resolve, reject) => {
+      const stream = fs.createReadStream(file.path);
+      const categories: IImportCategory[] = [];
+      const parseFile = csvParse();
 
-   stream.pipe(parseFile);
+      stream.pipe(parseFile);
 
-   parseFile.on('data', async (line) => {
-    const [name, description] = line;
-    categories.push({ name, description })
-   })
-    .on('end', () => {
-     resolve(categories);
-    }).on('error', (err) => {
-     reject(err);
+      parseFile.on('data', async (line) => {
+        const [name, description] = line;
+        categories.push({ name, description })
+      })
+        .on('end', () => {
+          fs.promises.unlink(file.path);
+          resolve(categories);
+        }).on('error', (err) => {
+          reject(err);
+        });
     });
-  });
- }
+  }
 
- async execute(file: Express.Multer.File): Promise<void> {
-  const categories = await this.loadCategories(file)
-  categories.map(async (category) => {
-   const { name, description } = category;
-   const existsCategory = this.categoriesRepository.findByName(name);
-   if (!existsCategory) {
-    this.categoriesRepository.create({ name, description })
-   }
-  })
- }
+  async execute(file: Express.Multer.File): Promise<void> {
+    const categories = await this.loadCategories(file)
+    categories.map(async (category) => {
+      const { name, description } = category;
+      const existsCategory = this.categoriesRepository.findByName(name);
+      if (!existsCategory) {
+        this.categoriesRepository.create({ name, description })
+      }
+    })
+  }
 }
 
 export { ImportCategoryUseCase }
