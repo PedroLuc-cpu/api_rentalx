19be2a328bbdd636f2ade85496af573866bbee7a
Pedro Lucas Santos <85002280+PedroLuc-cpu@users.noreply.github.com>
2023-08-21 23:49:04 -0300

    Criando token do usuário

--- src/Routes/index.ts
+++ src/Routes/index.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { categoriesRoutes } from "./categories.routes";
 import { specificationRoutes } from "./specifications.routes";
 import { usersRouters } from "./users.routes";
+import { authenticateRoutes } from "./authenticate.routes";
 
 
 const router = Router();
@@ -9,6 +10,7 @@ const router = Router();
 router.use("/categories", categoriesRoutes);
 router.use("/specifications", specificationRoutes);
 router.use("/users", usersRouters);
+router.use(authenticateRoutes);
 
 export { router }
 
---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,58 @@
+import { inject, injectable } from "tsyringe";
+import { IUserRepository } from "../../repositories/IUsersRepository";
+import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
+
+interface IRequest {
+ email: string;
+ password: string;
+}
+
+interface IResponse {
+ user: {
+  name: string;
+  email: string;
+ },
+ token: string;
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+
+ constructor(
+  @inject("UsersRepository")
+  private usersRespository: IUserRepository
+
+ ) { }
+
+ async execute({ email, password }: IRequest): Promise<IResponse> {
+  const user = await this.usersRespository.findByEmail(email);
+
+  if (!user) {
+   throw new Error("Email or password incorrect");
+  }
+
+  const passwordMatch = await compare(password, user.password);
+
+  if (!passwordMatch) {
+   throw new Error("Email or password incorrect");
+  }
+
+  const token = sign({}, "c10a1f8f263acdffbb48fe894fca4b43", {
+   subject: user.id,
+   expiresIn: "1d"
+  })
+
+  const tokenReturned: IResponse = {
+   token,
+   user: {
+    name: user.name,
+    email: user.email
+   }
+  }
+
+  return tokenReturned;
+ }
+}
+
+export { AuthenticateUserUseCase }
